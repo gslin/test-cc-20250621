fc3c15ca575f8e0000097c13022fd5e02b79073b
Gea-Suan Lin <gslin@gslin.org>
2025-06-21 09:07:16 +0800

    feat: ask CC to use tailwindcss.

--- app/index.js
+++ app/index.js
@@ -5,6 +5,7 @@ function createApp() {
   
   app.set('view engine', 'ejs');
   app.set('views', './views');
+  app.use(express.static('public'));
   
   app.get('/', (req, res) => {
     res.render('index', { 
